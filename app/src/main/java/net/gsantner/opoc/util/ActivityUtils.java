bab85a47645162c1667c0d611b153f912dc4b54d
Gregor Santner <gsantner@mailbox.org>
2017-11-17 12:50:35 +0100

    Update opoc

--- app/src/main/java/net/gsantner/opoc/util/ActivityUtils.java
+++ app/src/main/java/net/gsantner/opoc/util/ActivityUtils.java
@@ -12,8 +12,11 @@
 package net.gsantner.opoc.util;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -96,10 +99,10 @@ public class ActivityUtils extends net.gsantner.opoc.util.ContextUtils {
         }
     }
 
-    public void showSoftKeyboard(){
+    public void showSoftKeyboard() {
         InputMethodManager inputMethodManager = (InputMethodManager) _activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         if (_activity.getCurrentFocus() != null && _activity.getCurrentFocus().getWindowToken() != null) {
-            inputMethodManager.showSoftInput(_activity.getCurrentFocus() ,InputMethodManager.SHOW_FORCED);
+            inputMethodManager.showSoftInput(_activity.getCurrentFocus(), InputMethodManager.SHOW_FORCED);
         }
     }
 
@@ -135,5 +138,19 @@ public class ActivityUtils extends net.gsantner.opoc.util.ContextUtils {
             attrs.flags |= flag;
         }
         _activity.getWindow().setAttributes(attrs);
+    }
+
+    public void showRateOnGplayDialog() {
+        String pkgId = "details?id=" + _activity.getPackageName();
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, Uri.parse("market://" + pkgId));
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                (Build.VERSION.SDK_INT >= 21 ? Intent.FLAG_ACTIVITY_NEW_DOCUMENT : Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET) |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            _activity.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            _activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/" + pkgId)));
+        }
     }
 }
