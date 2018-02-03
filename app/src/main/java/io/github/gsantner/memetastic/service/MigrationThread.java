fc97de2c48a9389507117f15a54ad29816b375cb
Gregor Santner <gsantner@mailbox.org>
2018-02-03 21:47:15 +0100

    Update license notices

--- app/src/main/java/io/github/gsantner/memetastic/service/MigrationThread.java
+++ app/src/main/java/io/github/gsantner/memetastic/service/MigrationThread.java
@@ -1,3 +1,20 @@
+/*
+ * MemeTastic by Gregor Santner (http://gsantner.net)
+ * Copyright (C) 2016-2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.gsantner.memetastic.service;
 
 import android.content.Context;
@@ -10,10 +27,6 @@ import java.io.File;
 import io.github.gsantner.memetastic.R;
 import io.github.gsantner.memetastic.util.AppSettings;
 import io.github.gsantner.memetastic.util.PermissionChecker;
-
-/**
- * Created by gregor on 23.09.17.
- */
 
 public class MigrationThread extends Thread {
     private final Context _context;
