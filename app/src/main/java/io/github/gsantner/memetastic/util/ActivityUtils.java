ceee5702d460364bb8e7fe5fc8aff532a22c7c34
Gregor Santner <gsantner@mailbox.org>
2018-03-11 22:59:36 +0100

    Update opoc license headers

--- app/src/main/java/io/github/gsantner/memetastic/util/ActivityUtils.java
+++ app/src/main/java/io/github/gsantner/memetastic/util/ActivityUtils.java
@@ -22,6 +22,7 @@ import android.app.Activity;
 public class ActivityUtils extends net.gsantner.opoc.util.ActivityUtils {
     public ActivityUtils(Activity activity) {
         super(activity);
+
     }
 
     public static ActivityUtils get(Activity activity) {
