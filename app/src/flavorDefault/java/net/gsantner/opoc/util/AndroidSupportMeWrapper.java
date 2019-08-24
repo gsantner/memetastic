793b1a72466e91fffc4eb2aaa10bbaa7641a420c
Gregor Santner <gsantner@mailbox.org>
2019-08-24 14:48:21 +0200

    Update screenshots

--- app/src/flavorDefault/java/net/gsantner/opoc/util/AndroidSupportMeWrapper.java
+++ app/src/flavorDefault/java/net/gsantner/opoc/util/AndroidSupportMeWrapper.java
@@ -66,7 +66,7 @@ public class AndroidSupportMeWrapper extends ActivityUtils {
         }
 
         public boolean all14dRequest() {
-            return afterDaysTrue("all14dRequest", 14, 3);
+            return afterDaysTrue("all14dRequest", 31, 3);
         }
     }
 }
